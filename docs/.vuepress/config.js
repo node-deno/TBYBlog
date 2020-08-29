df481d3387cee1eb444ecca5b53f413c36f17e43
node_deno <node_deno@163.com>
2020-08-29 10:44:04 +0800

    完整发布vuepress-project

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,22 +9,41 @@ module.exports = {
 	themeConfig: {
 		// 这个主题是引用第三方的主题，这个主题有两种模式一种是普通模式，另一种是博客模式，在config.js的themeConfig中设置  type:'blog'即可
 		type: 'blog',
+
+		// nav左上角的logo
 		logo: '/images/logo.png',
-		nav,
+
+		// 头像设置
+		authorAvatar: '/images/faceImg.jpg',
+
+		// 导航栏顶部进行显示
+		nav: [
+			{ text: '时间轴', link: '/timeline/', icon: 'reco-date' },
+			{ text: 'github', link: 'https://github.com/node-deno/vuepress-project', icon: 'reco-github' }
+		],
+
+		// 侧边栏的配置
 		sidebar,
-		sidebarDepth: 1,
-		blogConfig: {
-			category: {
-				location: 1,     // 在导航栏菜单中所占的位置，默认2
-				text: '文章分类' // 默认文案 “分类”
-			},
-			tag: {
-				location: 5,     // 在导航栏菜单中所占的位置，默认5
-				text: 'Tag'      // 默认文案 “标签”
-			}
-		}
+
+		// 侧边栏是否打开
+		sidebarDepth: 'auto',
+		// 博客配置（这个是在nav导航栏的顶部进行显示）
+		// blogConfig: {
+		// 	category: {
+		// 		location: 1,     // 在导航栏菜单中所占的位置，默认2
+		// 		text: '文章分类' // 默认文案 “分类”
+		// 	},
+		// 	tag: {
+		// 		location: 5,     // 在导航栏菜单中所占的位置，默认5
+		// 		text: 'Tag'      // 默认文案 “标签”
+		// 	}
+		// },
+
+
 	},
 	author: 'TBY',
+
+	// 移动端优化设置
 	head: [
 		["link", { rel: "icon", href: "/favicon.ico" }],
 		["meta", { name: "node-deno", content: "width=device-width,initial-scale=1,user-scalable=no" }]
