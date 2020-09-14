dcc1349a21fd5770ca60da3df88c60ec0813d393
node_deno <node_deno@163.com>
2020-09-14 18:33:52 +0800

    20200914TBY

--- docs/.vuepress/config/sidebar.js
+++ docs/.vuepress/config/sidebar.js
@@ -1,19 +1,19 @@
 module.exports = {
-	'/views/': [
-		{
-			title: '工作开发总结',
-			collapsable: true,
-			children: [
-				'side1/page1',
-			]
-		},
-		{
-			title: 'JAVA内训总结',
-			collapsable: true,
-			children: [
-				'side2/page1',
-				'side2/page2',
-			]
-		}
-	]
+	// '/views/': [
+	// 	{
+	// 		title: '工作开发总结',
+	// 		collapsable: true,
+	// 		children: [
+	// 			'side1/page1',
+	// 		]
+	// 	},
+	// 	{
+	// 		title: 'JAVA内训总结',
+	// 		collapsable: true,
+	// 		children: [
+	// 			'side2/page1',
+	// 			'side2/page2',
+	// 		]
+	// 	}
+	// ]
 }
